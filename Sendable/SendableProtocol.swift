df5d6b4856a1364b7238810372506fca718f2c3c
Elaidzha1940 <eli080293@gmail.com>
2024-01-07 22:05:22 +0300

    Update SendablePViewModel

--- Sendable/SendableProtocol.swift
+++ Sendable/SendableProtocol.swift
@@ -9,17 +9,38 @@
 
 import SwiftUI
 
-class SendableP: ObservableObject {
+actor CurrentUserManager {
     
+    func updateDatbase(userInfo: MyUserInfo) {
+        
+    }
+}
+
+struct MyUserInfo {
+    let name: String
+}
+
+class SendablePViewModel: ObservableObject {
+    
+    let manager = CurrentUserManager()
+    
+    func updtaeCurrentUserInfo() async {
+        let info = MyUserInfo(name: "info")
+        
+        await manager.updateDatbase(userInfo: info)
+    }
 }
 
 struct SendableProtocol: View {
-    @StateObject private var viewModel = SendableP()
+    @StateObject private var viewModel = SendablePViewModel()
     
     var body: some View {
         
         VStack {
-          
+          Text("Hooo")
+        }
+        .task {
+            
         }
     }
 }
